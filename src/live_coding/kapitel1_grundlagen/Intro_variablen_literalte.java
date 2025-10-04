cd77be2428e310ce12522f075188bafdce0a479e
Mohammad <orabe.mhd@gmail.com>
2025-10-04 15:06:12 +0200

    add intro, variablen, literale, datentypen

---
+++ src/live_coding/kapitel1_grundlagen/Intro_variablen_literalte.java
@@ -0,0 +1,108 @@
+package live_coding.kapitel1_grundlagen;
+
+public class Intro_variablen_literalte {
+    public static void main(String[] args) {
+
+
+        // Indentation / Einrueckung
+        // Konvention
+
+
+        /*
+        Das ist mein kommentar hier.
+        Wir alle lieben java. programmieren ist einfach.
+        yeeeeey
+        */
+
+        // Das ist mein kommentar hier.
+
+        // Wir alle lieben java. programmieren ist einfach.
+        // yeeeeey
+        // datentype: nummer, text, symbol
+
+        // variable
+        //  - name (bezeichner): k
+        //  - wert (literal): '$'
+        //  - datentyp: char
+        int x = 5;
+        double y = 0.2;
+        String z = "ich liebe  7";
+        char k = '$';
+
+        // int f = "hi there"; // das ist falsch!!!
+        int f = 6; // DAS IST KORREKT
+        String f1 = "hi there";
+
+        // - _: underscore, unterstrich
+        int _zah_len_5_ = 2;
+        String sta4dt = "hamburg";
+        char s1 = '.';
+        String meineSpezieleVariable = "Mohammad"; // python
+        String meine_speziele_variable = "Mohammad"; // java
+        String meinespezielevariable = "Mohammad";
+
+        double nummer1 = 45.234325124353;
+        float nummer2 = 45.23432F;
+
+        // syntax : grammatik
+        // symmantik: bedeutung
+
+        byte temperature = 35;
+        int anzahlStudenten = 40000;
+        float koerpergroesse1 = 1.93f; // in meter
+        short koerpergroesse2 = 193; // in cm
+        int anzahlBewohner = 83000000;
+
+        // System.out.println("ich liebe java");
+        // System.out.println(40000);
+        // System.out.println('S');
+        System.out.println(anzahlBewohner); // 83000000
+        System.out.println("anzahlBewohner"); // anzahlBewohner
+
+
+
+
+// Erde {
+//     Germany {
+
+//         berlin {
+//             TU
+//             htw
+//         }
+
+
+
+//         hamburg {
+//             bht
+//         }
+
+
+//         muenchen {
+//             tum
+//         }
+
+
+
+//     }
+
+
+//     Syrien {
+
+//         lattakia {
+//             lattakia uni
+//             privates uni
+//         }
+
+
+
+//         homs {
+//             free syria
+//         }
+
+
+//     }
+// }
+
+
+    }
+}
